b916560599177ef9f60d0b8fd7c73eaef569e62a
Ruslan Rebo <ruslan.rebo@techno.ee>
2026-06-17 19:51:37 +0300

    feat: complete rick and morty app and deploy to gh-pages and surge

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Dynamically setting the base path so it works perfectly for both GitHub Pages and Surge
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
   plugins: [
     vue(),
     vueDevTools(),
